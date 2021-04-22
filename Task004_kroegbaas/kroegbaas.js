5083f2355bfe821b862a6e0ec0dee15c3a5b291c
JoseLuisSanchezMejia <82959294+JoseLuisSanchezMejia@users.noreply.github.com>
2021-04-22 18:09:31 +0200

    Add files via upload

---
+++ Task004_kroegbaas/kroegbaas.js
@@ -0,0 +1,31 @@
+const age=18;
+let clientAge=13;
+
+if (clientAge>= age){
+    console.log("Kom maar binnen");}
+else {
+    console.log("sorry je mag niet naar binnen, je bent te jong");
+}
+
+const isFemale = true;
+
+let nature =! isFemale;
+nature =isFemale;
+
+if (nature==isFemale){
+    console.log("Je bent een vrouw, kom maar binnen");}
+else {
+    console.log("sorry je mag niet naar binnen, je bent geen vrouw");
+}
+
+const driverstatus="bob";
+
+let driverstatusClient= "geen bob";
+
+
+if (driverstatusClient===driverstatus){
+    console.log("Je mag rijden. Wel thuis.");}
+else {
+    console.log("sorry je mag niet rijden. Bel een taxi");
+}
+
